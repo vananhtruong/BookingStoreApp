4a0f4963dc986d5e008eadc846547e56880e4a81
vananhtruong <truongvananh456@gmail.com>
2024-07-17 03:52:27 +0700

    vananh

--- BusnessObject/obj/Debug/net8.0/BusnessObject.AssemblyInfo.cs
+++ BusnessObject/obj/Debug/net8.0/BusnessObject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusnessObject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00bf2029c37ec57e61d9caf153ac67815a5c7ec2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusnessObject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusnessObject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
